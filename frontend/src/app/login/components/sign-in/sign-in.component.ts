a2a77231c4c38c1aefb5b5314f26004a0b6f736d
DESKTOP-6N9G5LS\USUARIO <aarroyaveduque20@gmail.com>
2024-09-13 18:37:42 -0500

    Se implementan de forma correcta las validaciones en el login

--- frontend/src/app/login/components/sign-in/sign-in.component.ts
+++ frontend/src/app/login/components/sign-in/sign-in.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { Router } from '@angular/router'
 
-import { LoginService } from 'src/app/services/login.service';
+import { LoginService } from 'src/app/services/login.service'
 
 @Component({
   selector: 'app-sign-in',
@@ -10,30 +10,35 @@ import { LoginService } from 'src/app/services/login.service';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
-  errorMessage?: string;
-  loginForm: FormGroup;
+  errorMessage?: string
+  public loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
 
-  constructor(private router: Router, private loginService: LoginService, private formBuilder: FormBuilder) {
-    this.loginForm = formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
-    });
-  }
+  constructor(private router: Router, private loginService: LoginService) { }
 
   redirectToSignUp() {
-    this.router.navigate(['sign-up']);
+    this.router.navigate(['sign-up'])
   }
 
   login() {
-    if (this.loginForm.valid) {
-      const { email, password } = this.loginForm.value;
-
-      this.loginService.login(email, password).subscribe(response => {
-        response ? this.router.navigate(['user']) : this.errorMessage = 'Invalid email or password';
-      }, error => {
-        console.log('Error in login request', error);
-        this.errorMessage = 'Error logging in. Please try again';
-      })
+    if(this.loginForm.invalid) {
+      this.errorMessage = 'Error logging in. Please try again'
+      return
     }
+
+    const email = this.loginForm.value.email as string // O const email = this.loginForm.value.email ?? ''
+    const password = this.loginForm.value.password as string // O const password = this.loginForm.value.password ?? ''
+
+    this.loginService.login(email, password).subscribe({
+      next: response => {
+        response ? this.router.navigate(['user']) : this.errorMessage = 'Invalid email or password'
+      },
+      error: error => {
+        console.log('Error in login request', error)
+        this.errorMessage = 'Error logging in. Please try again'
+      }
+    })
   }
 }
